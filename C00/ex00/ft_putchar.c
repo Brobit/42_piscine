8e9f593a6b540e3a22653a4dfff563db01d3b479
Brobit <83666070+Brobit@users.noreply.github.com>
2023-07-27 21:04:23 +0200

    Update ft_putchar.c

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: almarico <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/04 12:54:24 by almarico          #+#    #+#             */
-/*   Updated: 2023/07/04 13:43:57 by almarico         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -19,11 +7,6 @@ void	ft_putchar(char c)
 /*
 int	main(void)
 {
-				ft_putchar('a');
-				ft_putchar('l');
-				ft_putchar('a');
-				ft_putchar('n');
-				ft_putchar('n');
 				ft_putchar('1');
 				ft_putchar('2');
 				ft_putchar('3');
